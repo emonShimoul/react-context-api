3587f529e45cfde7ef1ac3894b0a02a7efdd4d3f
emonShimoul <emonshimoul@gmail.com>
2023-05-09 16:32:17 +0600

    use og context api

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,20 +1,37 @@
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Father from "../Father/Father";
 import Uncle from "../Uncle/Uncle";
 import "./Grandpa.css";
 
+export const RingContext = createContext("gold");
+export const MoneyContext = createContext(0);
+
 const Grandpa = () => {
   const ring = "diamond";
+  const [money, setMoney] = useState(0);
+
   return (
     <div className="grandpa">
       <h2>GrandPa</h2>
-      <section className="flex">
-        <Father ring={ring} />
-        <Uncle />
-        <Aunty ring={ring} />
-      </section>
+      <p>Has Money: {money}</p>
+      <MoneyContext.Provider value={[money, setMoney]}>
+        <RingContext.Provider value="golden ring">
+          <section className="flex">
+            <Father ring={ring} />
+            <Uncle />
+            <Aunty ring={ring} />
+          </section>
+        </RingContext.Provider>
+      </MoneyContext.Provider>
     </div>
   );
 };
 
 export default Grandpa;
+
+/*
+1. create a context and export
+2. create a provider and pass a value
+3. use useContext to recieve the value
+*/
--- src/components/Special/Special.jsx
+++ src/components/Special/Special.jsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
+import { RingContext } from "../Grandpa/Grandpa";
+
 /* eslint-disable react/prop-types */
 const Special = ({ ring }) => {
+  const angti = useContext(RingContext);
   return (
     <div>
       <h2>Special</h2>
       <p>
         <small>{ring}</small>
+        <br />
+        <small>{angti}</small>
       </p>
     </div>
   );
